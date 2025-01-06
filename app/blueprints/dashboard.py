9f25f9c5a190f11ad7ccba432565ccfb1e81e5e9
ashan-joseph <ashanjoseph13@gmail.com>
2025-01-06 19:59:51 +0530

    dashbaord api added

--- app/blueprints/dashboard.py
+++ app/blueprints/dashboard.py
@@ -9,7 +9,7 @@ import os
 bp = Blueprint("dashboard",__name__)
 
 @bp.route("/api/generate-dashboard-json/", methods=["GET"])
-def get_dashboard_data():
+def create_dashboard_data():
 
     try:
         total_customer_count =  CustomerModel.query.filter(CustomerModel.status == 1).with_entities(func.count(CustomerModel.id)).scalar()
@@ -23,6 +23,13 @@ def get_dashboard_data():
 
         result = db.session.query(CustomerModel.preferred_category, func.sum(CustomerModel.total_amount)).group_by(CustomerModel.preferred_category).limit(10).all()
 
+        chart_data = {
+            'high' : CustomerModel.query.filter(CustomerModel.segment_name == 'High Spenders').with_entities(func.count(CustomerModel.total_amount)).scalar(),
+            'moderate' : CustomerModel.query.filter(CustomerModel.segment_name == 'Moderate Spenders').with_entities(func.count(CustomerModel.total_amount)).scalar(),
+            'budget' : CustomerModel.query.filter(CustomerModel.segment_name == 'Budget Spenders').with_entities(func.count(CustomerModel.total_amount)).scalar(),
+            'low' : CustomerModel.query.filter(CustomerModel.segment_name == 'Low Engagement').with_entities(func.count(CustomerModel.total_amount)).scalar()
+        }
+        
         category_list = []
 
         for data in result:
@@ -37,16 +44,16 @@ def get_dashboard_data():
             "potantial_count"      : potential_count,
             "transaction_total"    : transaction_total,
             "low_key_users"        : low_key_users,
-            "best_mcc"             : category_list
+            "best_mcc"             : category_list,
+            "chart_data"           : chart_data
         }
 
         file_path = os.path.join('app', 'dashboard.json')
-        chart_path = os.path.join('app', 'pie.png')
 
         with open(file_path, "w") as json_file:
             json.dump(dashboard_data, json_file, indent=4)
 
-        return jsonify({"message": "Login successful!", "error": "false"})
+        return jsonify({"message": "JSON created successfully!", "error": "false"})
     except Exception as e:
         return jsonify({"error": "true",'message': f'An unexpected error occurred: {str(e)}'}), 500
     
@@ -56,3 +63,16 @@ def get_mcc_name(mcc_code):
         return category_data.name
     except Exception as e:
         return 'N/A'
+    
+@bp.route("/api/get-dashboard-data/", methods=["POST"])
+def get_dashboard_data():
+        
+    try:    
+        file_path = os.path.join('app', 'dashboard.json')
+
+        with open(file_path, "r") as json_file:
+            data = json.load(json_file)
+
+        return jsonify({"message": "Data retrieved successfully", "error": "false",'data':data})
+    except Exception as e:
+        return jsonify({"error": "true",'message': f'An unexpected error occurred: {str(e)}','data':'null'}), 500
